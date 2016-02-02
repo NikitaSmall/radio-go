ab2ce26e358979d2760fe25bc85be31c2adb47ee
NikitaSmall <nikitasosnov@yahoo.com>
2016-02-02 11:00:03 +0200

    implement basic usage of jplayer plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  // console.log('jquery works!');
+  $("#jquery_jplayer_1").jPlayer({
+		ready: function () {
+			$(this).jPlayer("setMedia", {
+				title: "Bubble",
+				mp3: "/music/Lana Del Rey - Chelsea Hotel No. 2.mp3"
+			});
+		},
+		swfPath: "/assets/js/",
+		supplied: "mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true,
+		remainingDuration: true,
+		toggleDuration: true
+	});
 });
